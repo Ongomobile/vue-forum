808e813f4a82b2407838e41665cd15e9f4a21ebf
ongomobile <ongomobile@gmail.com>
2022-02-09 11:18:00 -0800

    Add firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyCdkLZWkyMEQjRbMh45CLQ296y0h2GZidw',
+  authDomain: 'nuxt-app-56196.firebaseapp.com',
+  databaseURL: 'https://nuxt-app-56196.firebaseio.com',
+  projectId: 'nuxt-app-56196',
+  storageBucket: 'nuxt-app-56196.appspot.com',
+  messagingSenderId: '363126933388',
+  appId: '1:363126933388:web:d454baf1e2b33d0a0638a9'
+}
