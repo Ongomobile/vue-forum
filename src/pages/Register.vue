227147f92045f902dc43c55b00eee18e303e35cc
ongomobile <ongomobile@gmail.com>
2022-03-03 16:54:50 -0800

    Add vee-valaidate components and rules npm package

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,20 +1,7 @@
 <template>
   <div class="flex-grid justify-center">
     <div class="col-2">
-      <VeeForm
-        @submit="register"
-        class="card card-form"
-        :validation-schema="{
-          name: (value) => {
-            if (value && value.trim()) return true
-            return 'This is required'
-          },
-          username: (value) => {
-            if (value && value.trim()) return true
-            return 'This is required'
-          }
-        }"
-      >
+      <VeeForm @submit="register" class="card card-form">
         <h1 class="text-center">Register</h1>
 
         <div class="form-group">
@@ -25,7 +12,9 @@
             id="name"
             type="text"
             class="form-input"
+            rules="required"
           />
+          <VeeErrorMessage name="name" class="form-error" />
         </div>
 
         <div class="form-group">
@@ -36,7 +25,9 @@
             id="username"
             type="text"
             class="form-input"
+            rules="required"
           />
+          <VeeErrorMessage name="username" class="form-error" />
         </div>
 
         <div class="form-group">
@@ -92,12 +83,7 @@
   </div>
 </template>
 <script>
-import { Form, Field } from 'vee-validate'
 export default {
-  components: {
-    VeeForm: Form,
-    VeeField: Field
-  },
   data() {
     return {
       avatarPreview: null,
